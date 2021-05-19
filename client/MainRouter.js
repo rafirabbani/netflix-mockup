5e7d90385158ca449a5611a17d32b9873ee3d7d0
Rafi Rabbani <rafirabbani103@gmail.com>
2021-05-19 10:05:36 +0700

    add auth guard to mainrouter

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Switch, Route } from "react-router-dom";
+import axios from 'axios'
+import { Switch, Route, Redirect } from "react-router-dom";
 import MainLayout from './views/MainLayout'
 import Dashboard from './views/Dashboard';
 import Home from './views/Home'
@@ -9,9 +10,30 @@ import Comments from './views/Comments/Comments'
 import Users from './views/Users/Users'
 import Login from './views/Auth/Login'
 
-
+const authGuard = (Component) => {
+  const element = <Component />
+  let auth = JSON.parse(localStorage.getItem('data'))
+  if (auth === null) {
+    return <Redirect to="/netflix-mockup/login" />
+  }
+  else {
+      axios.post(`/api/auth/requiresignin`, {}, {
+        headers: { 
+          'Authorization': `Bearer ${auth.token}`}
+      })
+      .then(result => {
+        console.log(result)
+      })
+      .catch (err => {
+        console.log(err.response)
+        return <Redirect to='/netflix-mockup/login' />
+      })
+      return element
+  } 
+}
 
 const MainRouter = () => {
+  
   return (
   <>
     <Switch>
